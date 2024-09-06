a64ff5987823b7b2f0efbb65bd735fc088714253
green-new <tristenwoodruff2@gmail.com>
2024-09-05 22:49:52 -0400

    Consolidating main json header file

--- json-reader/json.hpp
+++ json-reader/json.hpp
@@ -1,49 +1,18 @@
 /**
  * @file json.hpp
- *
- * @brief JSON classes for objects, arrays, numbers, values, and literals
- *
+ * @brief Combined header file
  * @addtogroup json
- *
  * @author green-new
  * Contact: tristenwoodruff2@gmail.com
  */
 
-#include <bit>
-#include <concepts>
-#include <string>
-#include <sstream>
-#include <unordered_map>
-#include <map>
-#include <type_traits>
-#include <memory>
-#include <vector>
-#include <cmath>
-#include <optional>
-#include <stdexcept>
-#include <format>
-#include <functional>
-#include <utility>
-#include <stdfloat>
-#include "iterable.hpp"
-
 #pragma once
 
-/**
-* List of todos.
-* @todo Implementation file for all implementations.
-* @todo Header file for builders, iterable, etc..
-* @todo Forward declare all objects.
-*/
-
-namespace json {
-
-	// Forward declarations
-	class value;
-	class number;
-	class boolean;
-	class array;
-	class object;
-	class null;
-
-}
+#include "array.hpp"
+#include "boolean.hpp"
+#include "null.hpp"
+#include "number.hpp"
+#include "object.hpp"
+#include "root.hpp"
+#include "string.hpp"
+#include "value.hpp"
